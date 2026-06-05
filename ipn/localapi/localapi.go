27d6387412cc71c379a7f83d82b226ccd2abb870
Simon Law <sfllaw@tailscale.com>
2026-06-05 07:52:21 -0700

    fixup! cmd/tailscale/cli: add `tailscale exit-node suggest --force-probe`
    @amalscale pointed out that we had agreed on what it means to
    routecheck.Probe with a zero timeout, so the serveSuggestExitNode
    handler ought to support both zero and default timeouts.
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

--- ipn/localapi/localapi.go
+++ ipn/localapi/localapi.go
@@ -1837,6 +1837,13 @@ func dnsMessageTypeForString(s string) (t dnsmessage.Type, err error) {
 var HookRouteCheckRefresh feature.Hook[func(*ipnlocal.LocalBackend, context.Context, time.Duration) error]
 
 // serveSuggestExitNode serves a POST endpoint for returning a suggested exit node.
+// If the probe query parameter is true,
+// then a new routecheck report will be probed
+// so that the suggested exit node isn’t based on stale reachability data.
+// Combined with probe=true:
+// if the timeout query parameter is 0, any probes will immediately timeout;
+// if the timeout is positive, probes will take that duration before timing out;
+// if the timeout is negative, probes will use the default routecheck timeout.
 func (h *Handler) serveSuggestExitNode(w http.ResponseWriter, r *http.Request) {
 	if !buildfeatures.HasUseExitNode {
 		http.Error(w, feature.ErrUnavailable.Error(), http.StatusNotImplemented)
@@ -1868,7 +1875,7 @@ func (h *Handler) serveSuggestExitNode(w http.ResponseWriter, r *http.Request) {
 		if !defBool(r.FormValue("probe"), false) {
 			break
 		}
-		timeout := defDuration(r.FormValue("timeout"), 0)
+		timeout := defDuration(r.FormValue("timeout"), -1)
 		routecheckRefresh := HookRouteCheckRefresh.GetOrNil()
 		if routecheckRefresh == nil {
 			break
